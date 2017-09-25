c21e3079899c342ef83b8d6a203379c25fa5ca04
Austin Philp <austinphilp@gmail.com>
2017-09-24 19:55:06 -0700

    first commit

---
+++ sudoku.py
@@ -0,0 +1,107 @@
+import sys
+
+class Square:
+    possibilities = []
+
+    def isSolved(self):
+        return self.numberOfPossibilities() == 1
+
+    def numberOfPossibilities(self):
+        return len(self.activePossibilities())
+        
+    def activePossibilities(self):
+        return [x for x in self.possibilities if x != None]
+
+    def getValue(self):
+        if not self.isSolved():
+            return None 
+        else:
+            return [x for x in self.possibilities if x != None][0]
+
+    def assignVal(self, val):
+        if val > 0:
+            for i in range(0,9):
+                if self.possibilities[i] != val:
+                    self.possibilities[i] = None
+
+    def eliminatePossibility(self, num):
+        self.possibilities[num-1] = None
+        return self.isSolved()
+
+    def getPossibilitiesString(self):
+        return ''.join(str(num) for num in self.activePossibilities())
+
+    def __init__(self):
+        self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class Grid:
+    Squares = []
+
+    def smartElimination(self):
+        for row in range(0,9):
+            for col in range(0,9):
+                if self.Squares[row][col].isSolved():
+                    self.removePossibilityForRow(row, self.Squares[row][col].getValue())
+                    self.removePossibilityForCol(col, self.Squares[row][col].getValue())
+                    self.removePossibilityForSubGrid( row, col, self.Squares[row][col].getValue())
+                
+
+    def removePossibilityForRow(self, row, val):
+        for square in self.Squares[row]:
+            if not square.isSolved():
+                if square.eliminatePossibility(val):
+                    self.removePossibilityForRow(row, square.getValue())
+
+
+    def removePossibilityForCol(self, col, val):
+        for row in self.Squares:
+            if not row[col].isSolved():
+                if row[col].eliminatePossibility(val):
+                    self.removePossibilityForCol(col, row[col].getValue())
+
+    def removePossibilityForSubGrid(self, squareRow, squareCol, val):
+        for row in range((squareRow/3)*3, ((squareRow/3)*3)+3):
+            for col in range((squareCol/3)*3, ((squareCol/3)*3)+3):
+                square = self.Squares[row][col]
+                if not self.Squares[row][col].isSolved():
+                    if self.Squares[row][col].eliminatePossibility(val):
+                        self.removePossibilityForCol(col, self.Squares[row][col].getValue())
+                    
+
+
+    def display(self):
+        width = -1
+
+        for row in self.Squares:
+            for square in row:
+                width = max([square.numberOfPossibilities(), width])
+
+        line = '+'.join(['-'*width*3]*3)
+
+        for row in range(0,9):
+            print ''.join(self.Squares[row][col].getPossibilitiesString().center(width)+('|' if col in [2,5] else '') for col in range(0,9))
+            if row in [2,5]: print line
+                
+    def __init__(self):
+        for x in range(0,9):
+            self.Squares.append([])
+            for y in range(0,9):
+                self.Squares[x].append(Square())
+
+GridNum = 0
+grid = Grid()
+row = 0
+col = 0
+with open("p096_sudoku.txt") as f:
+    for line in f.readlines()[((GridNum*9)+1 ):GridNum+8]:
+        for num in line:
+            if(num.isdigit()):
+                grid.Squares[row][col].assignVal(int(num))
+            col += 1
+        row += 1
+        col = 0
+print "=========== Before ==========="
+grid.display()
+grid.smartElimination()
+print "=========== After ==========="
+grid.display()
