49c316c268e17bfec2fb1fe58b66a6c1130a0fd3
Austin Philp <austin@austinphilp.com>
2017-09-26 21:49:12 +0000

    finish refactor

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,7 @@
 import sys
 from copy import deepcopy
-from copy import copy
-from time import sleep
+
+PUZZLE_FILE = "p096_sudoku.txt"
 
 class Square:
     possibilities = []
@@ -29,10 +29,6 @@ class Square:
                 if self.possibilities[i] != val:
                     self.possibilities[i] = None
 
-    def addPosibility(self, val):
-        self.possibilities[val-1] = val
-        return self.isSolved()
-
     def isValid(self):
         return self.numberOfPossibilities() > 0
 
@@ -45,45 +41,52 @@ class Square:
     def getPossibilitiesString(self):
         return ''.join(str(num) for num in self.activePossibilities())
 
-    def setCoord(self, x, y): 
+    def __init__(self, x, y, val):
+        self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.x = x
         self.y = y
-
-    def __init__(self):
-        self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.x == other.x and self.y == other.y
-
-    def __eq__(self, other):
-        return not slef.__eq__(other)
-
+        self.assignVal(val)
 
 class Grid:
     Squares = []
     
     @staticmethod
     def Solve(grid):
-        if all(square.isSolved() for square in flattenArray(deepcopy(grid.Squares))):
+        # if the grid is solved, 
+        if grid.isSolved():
             return grid
-        if not grid.SquaresAreValid():
+        # if the grid is now invalid, return None
+        if not grid.isValid():
             return None
 
+        # Get the square with the least amount of possible values
         square = grid.getMostSolvedSquare()
         row, col = square.x, square.y
+
+        # try each possible value 
         for possibility in square.activePossibilities():
-            grid.display()
+            #Backup a copy of the squares to restore if branch fails
             backupSquares = deepcopy(grid.Squares)
+
+            #Assign the possible value and perform a smart elimination
             grid.Squares[row][col].assignVal(possibility)
             grid.smartElimination()
-            grid.display()
+
+            #Attempt to solve the new grid
             result = Grid.Solve(grid)
+            
+            # result will only be None if the grid was not valid
             if result is not None:
                 return result
             else:
                 grid.Squares  = backupSquares
         return None
 
+    # Return True if all squares have only one possible value
+    def isSolved(self):
+        return all(square.isSolved() for square in flattenArray(deepcopy(self.Squares)))
+
+    # Return the square that has the least possible values
     def getMostSolvedSquare(self):
         minVal = 9
         minSquare = None
@@ -95,93 +98,80 @@ class Grid:
                         minVal = self.Squares[row][col].numberOfPossibilities()
         return minSquare
 
+    # Eliminate possibilities for squares based on the known values of other squares following the rules of sudoku
     def smartElimination(self):
         solved = [square for square in flattenArray(self.Squares) if square.isSolved()]
         while len(solved) > 0:
             square = solved.pop(0)
-            solved += Grid.RemovePossibilityForRow( self.Squares, square )
-            solved += Grid.RemovePossibilityForCol( self.Squares, square )
-            solved += Grid.RemovePossibilityForSubGrid( self.Squares, square )
+            solved += self.RemovePossibilityForRow( square )
+            solved += self.RemovePossibilityForCol( square )
+            solved += self.RemovePossibilityForSubGrid( square )
 
-    def SquaresAreValid(self):
-        for row in range(0,9):
-            for col in range(0,9):
-                if not self.Squares[row][col].isValid():
-                    print "NOT VALID"
-                    return False
-        return True
+    # Return true if every square in the grid has at least 1 value
+    def isValid(self):
+        return all(square.isValid() for square in flattenArray(deepcopy(self.Squares)))
 
-    @staticmethod
-    def RemovePossibilityForRow(squares, square):
+    # Remove the value of the given square from the possibilities of every other square in its row
+    def RemovePossibilityForRow(self, square):
         solved = []
         row = square.x
         for col in range(0,9):
             if not col == square.y:
-                if squares[row][col].eliminatePossibility(squares[square.x][square.y].getValue()):
-                    if not squares[row][col].isValid(): 
+                if self.Squares[row][col].eliminatePossibility(self.Squares[square.x][square.y].getValue()):
+                    if not self.Squares[row][col].isValid(): 
                         print "FOUND INVALID"
                         return None
                     else:
-                        solved.append(deepcopy(squares[row][col]))
+                        solved.append(deepcopy(self.Squares[row][col]))
         return solved
 
-    @staticmethod
-    def RemovePossibilityForCol(squares, square):
+    # Remove the value of the given square from the possibilities of every other square in its column
+    def RemovePossibilityForCol(self, square):
         solved = []
         col = square.y
         for row in range(0,9):
-            if not squares[row][col].x == square.x:
-                if squares[row][col].eliminatePossibility(squares[square.x][square.y].getValue()):
-                    if not squares[row][col].isValid(): 
+            if not self.Squares[row][col].x == square.x:
+                if self.Squares[row][col].eliminatePossibility(self.Squares[square.x][square.y].getValue()):
+                    if not self.Squares[row][col].isValid(): 
                         print "FOUND INVALID"
                         return None
                     else:
-                        solved.append(deepcopy(squares[row][col]))
+                        solved.append(deepcopy(self.Squares[row][col]))
         return solved
 
-    @staticmethod
-    def RemovePossibilityForSubGrid(squares, square):
+    # Remove the value of the given square from the possibilities of every other square in its subgrid
+    def RemovePossibilityForSubGrid(self, square):
         solved = []
         if square.getValue() == None:
             return 
         for row in range((square.x/3)*3, ((square.x/3)*3)+3):
             for col in range((square.y/3)*3, ((square.y/3)*3)+3):
                 if row != square.x and col != square.y:
-                    if squares[row][col].eliminatePossibility(squares[square.x][square.y].getValue()):
-                        if not squares[row][col].isValid(): 
+                    if self.Squares[row][col].eliminatePossibility(self.Squares[square.x][square.y].getValue()):
+                        if not self.Squares[row][col].isValid(): 
                             print "FOUND INVALID"
                             return None
                         else:
-                            solved.append(deepcopy(squares[row][col]))
+                            solved.append(deepcopy(self.Squares[row][col]))
         return solved
 
-    def bruteForce(self): 
-        squares = Grid.GetCorrectSquares(deepcopy(self.Squares))
-        Grid.Display(squares)
-        self.Squares = squares
-
+    # Print the grid out, displaying every possible value for that square
     def display(self):
-        Grid.Display(self.Squares)
-
-    @staticmethod
-    def Display(squares):
-        print "==============================="
-        width = -1
-        for row in squares:
-            for square in row:
-                width = max([square.numberOfPossibilities(), width])
-        line = '+'.join(['-'*width*3]*3)
+        line = '+'.join(['-'*3]*3)
         for row in range(0,9):
-            print ''.join(squares[row][col].getPossibilitiesString().center(width)+('|' if col in [2,5] else '') for col in range(0,9))
+            print ''.join(str(self.Squares[row][col].getValue() or "*")+('|' if col in [2,5] else '') for col in range(0,9))
             if row in [2,5]: print line
-        print "==============================="
                 
-    def __init__(self):
-        for x in range(0,9):
+    # construct the grid from a given array of lines from the sudoku file
+    def __init__(self, lines):
+        for line in lines:
             self.Squares.append([])
-            for y in range(0,9):
-                self.Squares[x].append(Square())
+            for num in [x for x in line if x.isdigit()]:
+                row = len(self.Squares)-1
+                col = len(self.Squares[row])
+                self.Squares[row].append(Square(row,col,int(num)))
 
+# convert a 2d array into a 1d array
 def flattenArray(list):
     newList = []
     for row in range(0,len(list)):
@@ -189,26 +179,22 @@ def flattenArray(list):
             newList.append(list[row][col])
     return newList
 
-GridNum = 30
-grid = Grid()
-row = 0
-col = 0
-with open("p096_sudoku.txt") as f:
-    start = (GridNum-1) * 9 + GridNum
-    end = start + 9
-    for line in f.readlines()[start:end]:
-        for num in line:
-            if(num.isdigit()):
-                grid.Squares[row][col].assignVal(int(num))
-                grid.Squares[row][col].setCoord(row, col)
-                print "setting coord", row, col
-            col += 1
-        row += 1
-        col = 0
-print "=========== Before ==========="
+if len(sys.argv) < 2:
+    print "ERR: No puzzle number provided"
+    print "Example Usage : $ python pydoku.py <PUZZLE_NUMBER>"
+    exit()
+else: 
+    try:
+        GridNum = int(sys.argv[1])
+    except ValueError:
+        print "Argument provided is invalid, defaulting to 1"
+start = (GridNum-1)*9+GridNum
+end = start + 9
+with open(PUZZLE_FILE) as f:
+    grid = Grid(f.readlines()[start:end])
+print "=========== Puzzle ==========="
 grid.display()
+raw_input("Press Enter to reveal solution")
+print "\n=========== Solution ==========="
 grid.smartElimination()
-print "=========== After ==========="
-grid.display()
-
 Grid.Solve(grid).display()
