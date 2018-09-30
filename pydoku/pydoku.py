773002ea92dc0dae36dbd41da1394ccbda1d18c5
Austin Philp <austin@ticketguardian.net>
2018-09-30 13:19:14 -0700

    Fix main method implementation

--- pydoku/pydoku.py
+++ pydoku/pydoku.py
@@ -4,32 +4,26 @@ import constants
 from grid import Grid
 
 
-if len(sys.argv) < 2:
-    print("ERR: No puzzle number provided")
-    print("Example Usage : $ python pydoku.py <PUZZLE_NUMBER>")
-    exit()
-else:
-    try:
-        puzzle_exists = (
-            int(sys.argv[1]) <= constants.MAX_PUZZLE_NUM and
-            int(sys.argv[1]) >= constants.MIN_PUZZLE_NUM
-        )
-        if puzzle_exists:
-            GridNum = int(sys.argv[1])
-            with open(constants.PUZZLE_FILE) as f:
-                grid = Grid(f.readlines()[(GridNum*10)-9:GridNum*10])
+def main(grid_num):
+    if len(sys.argv) < 2:
+        print("ERR: No puzzle number provided")
+        print("Example Usage : $ python pydoku.py <PUZZLE_NUMBER>")
+        exit()
+    else:
+        with open(constants.PUZZLE_FILE) as f:
+            grid = Grid(f.readlines()[(grid_num*10)-9:grid_num*10])
 
-            print("=========== Unsolved Puzzle ===========")
-            grid.display()
-            input("Press Enter to reveal solution")
-            print("\n=========== Solution ===========")
-            grid.solve().display()
-        else:
-            print(
-                "Puzzle Number is out of range, "
-                "please choose a puzzle from 1 - 50"
-            )
-    except ValueError:
-        print("Argument provided is invalid, defaulting to 1")
+        print("=========== Unsolved Puzzle ===========")
+        grid.display()
+        input("Press Enter to reveal solution")
+        print("\n=========== Solution ===========")
+        grid.solve().display()
 
-# Read the relevant lines from the sudoku file and construct a grid with them
+if __name__ == "__main__":
+    grid_num = int(sys.argv[1])
+    puzzle_exists = (
+        grid_num <= constants.MAX_PUZZLE_NUM and
+        grid_num >= constants.MIN_PUZZLE_NUM
+    )
+    assert puzzle_exists
+    main(grid_num)
