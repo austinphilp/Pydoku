98d62d055c334decedc6023efed9ee84d6ccb471
Austin Philp <austin@ticketguardian.net>
2018-11-27 19:31:23 -0800

    Create basic testing structure

--- pydoku/pydoku.py
+++ pydoku/pydoku.py
@@ -1,29 +1,20 @@
 import sys
 
-import constants
-from grid import Grid
+from utils import load_grid_from_file
 
 
 def main(grid_num):
+    grid = load_grid_from_file(grid_num)
+    print("=========== Unsolved Puzzle ===========")
+    grid.display()
+    input("Press Enter to reveal solution")
+    print("\n=========== Solution ===========")
+    grid.solve().display()
+
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("ERR: No puzzle number provided")
         print("Example Usage : $ python pydoku.py <PUZZLE_NUMBER>")
         exit()
     else:
-        with open(constants.PUZZLE_FILE) as f:
-            grid = Grid(f.readlines()[(grid_num*10)-9:grid_num*10])
-
-        print("=========== Unsolved Puzzle ===========")
-        grid.display()
-        input("Press Enter to reveal solution")
-        print("\n=========== Solution ===========")
-        grid.solve().display()
-
-if __name__ == "__main__":
-    grid_num = int(sys.argv[1])
-    puzzle_exists = (
-        grid_num <= constants.MAX_PUZZLE_NUM and
-        grid_num >= constants.MIN_PUZZLE_NUM
-    )
-    assert puzzle_exists
-    main(grid_num)
+        main(grid_num=int(sys.argv[1]))
---
+++ pydoku/tests.py
@@ -0,0 +1,12 @@
+from utils import load_grid_from_file
+
+
+def _test_puzzle(grid_num):
+    grid = load_grid_from_file(grid_num)
+    grid.solve()
+    return grid.isSolved()
+
+
+def test_all_puzzles_work():
+    for grid_num in range(1, 50):
+        assert _test_puzzle(grid_num)
