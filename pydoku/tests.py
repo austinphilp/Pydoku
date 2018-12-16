b328a556d6ee90accadd0e04ae58598061b0bb1d
Austin Philp <austinphilp@gmail.com>
2018-12-15 18:48:15 -0800

    UPdate testing documentation

--- pydoku/tests.py
+++ pydoku/tests.py
@@ -9,5 +9,7 @@ def _test_puzzle(grid_num):
     assert solver.is_solved
 
 
+# Auto generate puzzle tests to test each puzzle in the puzzle file
+# independently
 for n in range(0, 50):
     locals()[F"test_puzzle_{n}"] = lambda: _test_puzzle(n)
