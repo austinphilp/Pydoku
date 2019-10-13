13422a2de56a618a36a23b3fbf28b91be162687c
Austin Philp <austinphilp@gmail.com>
2019-10-13 09:54:26 -0700

    Add docs to utils.py

--- pydoku/utils.py
+++ pydoku/utils.py
@@ -5,6 +5,13 @@ from grid import Grid
 
 
 def load_grid_from_file(grid_num):
+    """
+    Arguments:
+        grid_num(int) - the number of the grid as specified in
+        puzzles/p096_sudoku.txt, grid number must be between 0 and 50
+    Returns:
+        Grid - Grid loaded from the puzzles/p096_sudoku.txt
+    """
     puzzle_exists = (
         grid_num <= constants.MAX_PUZZLE_NUM and
         grid_num >= constants.MIN_PUZZLE_NUM
